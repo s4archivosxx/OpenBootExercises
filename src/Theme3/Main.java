96020d06ccd85ce8b4982599a2338806ccf258f7
S4Archivos <jordan_vasquez_2019@hotmail.com>
2023-02-28 11:19:11 -0500

    Theme3 Finished

--- src/Theme3/Main.java
+++ src/Theme3/Main.java
@@ -3,5 +3,26 @@ package Theme3;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        // Primera parte:
+        int resultado = sum(2, 5, 3);
+        System.out.println("Resultado es: " + resultado);
+
+        // Segunda parte:
+        coche miCoche = new coche();
+        miCoche.incrementarPuertas(4);
+
+        System.out.println("Número de puertas: " + miCoche.puertas);
+    }
+
+    public static int sum(int a, int b, int c){
+        return a + b + c;
+    }
+}
+
+class coche{
+    public int puertas = 0;
+    public void incrementarPuertas(int valor){
+        this.puertas = valor;
     }
 }
